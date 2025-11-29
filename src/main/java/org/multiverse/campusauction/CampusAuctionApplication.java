1e503efdc98c6d3a3d01b119034285c860a7a69d
multuverse <115260876+duoyuanyuzhou@users.noreply.github.com>
2025-11-29 14:45:26 +0800

    项目初始化

---
+++ src/main/java/org/multiverse/campusauction/CampusAuctionApplication.java
@@ -0,0 +1,13 @@
+package org.multiverse.campusauction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusAuctionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CampusAuctionApplication.class, args);
+    }
+
+}
