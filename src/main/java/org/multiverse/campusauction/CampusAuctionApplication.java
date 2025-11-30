703a468c94932ebed1322456c6b885938c3be5b3
multuverse <115260876+duoyuanyuzhou@users.noreply.github.com>
2025-11-30 09:30:38 +0800

    添加MapperScan

--- src/main/java/org/multiverse/campusauction/CampusAuctionApplication.java
+++ src/main/java/org/multiverse/campusauction/CampusAuctionApplication.java
@@ -1,9 +1,11 @@
 package org.multiverse.campusauction;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("org.multiverse.campusauction.mapper")
 public class CampusAuctionApplication {
 
     public static void main(String[] args) {
